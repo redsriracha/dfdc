386a935ab4671c1d03a99277ab927dfea5aeb01e
Richard Vu Tran <redsriracha@gmail.com>
2020-03-07 01:42:01 -0600

    CP2: is_same_image()

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # or any {'0', '1', '2'}
 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"  # or any {'0', '1', '2'}
+
+import json
 from tqdm import tqdm
 import tensorflow as tf
-tf.config.list_physical_devices('GPU')
+
+tf.config.list_physical_devices("GPU")
 
 DATASET_NAME = "dfdc_train_part_0"
 
--- extract_spectrogram.py
+++ extract_spectrogram.py
@@ -1,18 +1,48 @@
 from config import *
 
-if __name__ == '__main__':
+
+def mp4_to_spectrogram(video_filepath):
+    video_filepath = video_filepath.decode("utf-8")
+    filename_ext = os.path.basename(video_filepath)
+    filename = os.path.splitext(filename_ext)[0]
+    dest = os.path.join(spectrogram_dir, filename + ".png")
+    if os.path.exists(dest):
+        return
+    cmd = "ffmpeg -i " + video_filepath + " -lavfi showspectrumpic=s=hd480:legend=0,format=yuv420p " + dest
+    # print(cmd)
+    os.system(cmd)
+
+
+def is_equal_image(img_1, img_2):
+    return tf.math.reduce_all(tf.equal(img_1, img_2))
+
+
+if __name__ == "__main__":
     spectrogram_dir = VIDEO_DIR_PATH + "_spectrogram"
     if not os.path.exists(spectrogram_dir):
         os.mkdir(spectrogram_dir)
 
-    file_ds = tf.data.Dataset.list_files(os.path.join(VIDEO_DIR_PATH, "*.mp4"), shuffle=False)
-    for file in tqdm(file_ds.as_numpy_iterator()):
-        file = file.decode("utf-8")
-        filename_ext = os.path.basename(file)
-        filename = os.path.splitext(filename_ext)[0]
-        dest = os.path.join(spectrogram_dir, filename + ".png")
-        if os.path.exists(dest):
-            continue
-        cmd = "ffmpeg -i " + file + " -lavfi showspectrumpic=s=hd480:legend=0,format=yuv420p " + dest
-        # print(cmd)
-        os.system(cmd)
+    video_filepath_ds = tf.data.Dataset.list_files(os.path.join(VIDEO_DIR_PATH, "*.mp4"), shuffle=False)
+    for video_filepath in tqdm(video_filepath_ds.as_numpy_iterator()):
+        mp4_to_spectrogram(video_filepath=video_filepath)
+
+    data_file = open("dataset/dfdc_train_part_0/metadata.json")
+    data = json.load(data_file)
+    for item in data:
+        if not data[item].get("original") == None:
+            img_1_path = os.path.splitext(item)[0] + ".png"
+            img_1 = tf.io.read_file(os.path.join(spectrogram_dir, img_1_path))
+            img_1 = tf.image.decode_png(img_1)
+
+            img_2_path = os.path.splitext(data[item].get("original"))[0] + ".png"
+            img_2 = tf.io.read_file(os.path.join(spectrogram_dir, img_2_path))
+            img_2 = tf.image.decode_png(img_2)
+
+            result = is_equal_image(img_1, img_2)
+            result = result.numpy()
+
+        else:
+            result = "NA"
+        print("{0:6s}".format(str(result)), item, data[item].get("original"))
+
+    # spectrogram_ds = tf.data.Dataset.list_files(os.path.join(spectrogram_dir, "*"), shuffle=False)
